14f23f4a8318b59d01f148f09811085272ea7b5f
radialcode-mohit <mohitgirdhar9034@gmail.com>
2023-02-24 21:12:53 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const navicon = document.getElementById("naviconn");
+const ul = document.getElementById("checked");
+navicon.addEventListener("click", function () {
+    ul.classList.toggle("show")
+});
